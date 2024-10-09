6f4c6acd3b4a5bf8c88186321610ddfaa563f210
yogesh saini <ys8470saini@gmail.com>
2024-10-09 16:51:53 +0530

    register page completed

--- SMS-Application-Layer/src/main/java/com/sms/SmsApplicationLayerApplication.java
+++ SMS-Application-Layer/src/main/java/com/sms/SmsApplicationLayerApplication.java
@@ -2,8 +2,10 @@ package com.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class SmsApplicationLayerApplication {
 
 	public static void main(String[] args) {
